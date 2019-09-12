75114627e91a7bcca75c39d32d045eec5a036b80
SriRamya01 <adusumilli.shree@gmail.com>
2019-09-12 19:07:00 -0400

    --first commit

---
+++ CoinsUX/Models/Coins.cs
@@ -0,0 +1,12 @@
+﻿namespace CoinsUX.Models
+{
+    
+    public class Coins
+    {
+        public string ID { get; set; }
+
+        public string Symbol { get; set; }
+
+        public string Name { get; set; }
+    }
+}
